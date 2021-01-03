2fb38a1a8d0f0758f519618d6fcde66b21b955d5
Taha M <tahanaeem12@hotmail.com>
2021-01-03 07:06:40 -0500

    inital commit

---
+++ src/location_monitor/scripts/location_monitor_node.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+import math
+
+landmarks = []
+landmarks.append(("TopCube", 2.45, 0.30))
+landmarks.append(("RightCube", 1.59, -1.96))
+landmarks.append(("LeftCube", 1.38, 1.96))
+
+def distance(x1, y1, x2, y2):
+	xd = x1 - x2
+	yd = y1 - y2
+	return math.sqrt(xd**2 + yd**2)
+
+def callback(msg):
+	x = msg.pose.pose.position.x
+	y = msg.pose.pose.position.y
+	closest_name = None
+	closest_distance = None 
+	for name, x_lm, y_lm in landmarks:
+		dist = distance(x, y, x_lm, y_lm)
+		if closest_distance is None or dist < closest_distance:
+			closest_name = name 
+			closest_distance = dist
+	rospy.loginfo('closest: {}'.format(closest_name))
+
+def main():
+	rospy.init_node('location_montior')
+	rospy.Subscriber("/odom", Odometry, callback)
+	rospy.spin()
+
+if __name__ == '__main__':
+	main()
+
